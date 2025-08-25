586a30097e7c2e4a0c8e0e6366c863a1a3e4f851
Ludmila Mizus <ludmila.mizus@gmail.com>
2025-08-25 21:02:36 +0300

    Session 2 over

--- world.gd
+++ world.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+var vx = 0
+
+
 func _process(delta: float):
 	var c = $Character
-	print(c.position.x)
-	c.play()
-	c.position.x += 1
+	print("Character.x " + str(c.position.x))
+	c.position.x += vx
 	if c.position.x >= 315:
 		c.position.x -= 320
 	
@@ -14,3 +16,21 @@ func _init():
 #func _draw():
 	#i += 1
 	#print("draw() " + str(i))
+
+func _input(event: InputEvent):
+	var left = event.is_action_pressed("ui_left")
+	var right = event.is_action_pressed("ui_right")
+	var c = $Character
+	
+	if left:
+		vx = -1
+		c.play()
+		
+	if right:
+		vx = 1
+		c.play()
+	
+	if event.is_action_released("ui_left") \
+	   or event.is_action_released("ui_right"):
+		vx = 0
+		c.stop()
