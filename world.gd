dbc1a6fce9ecb560aea897cff876825d1134be13
Ludmila Mizus <ludmila.mizus@gmail.com>
2025-09-09 21:00:44 +0300

    Session 3 over: The character is limited to the screen

--- world.gd
+++ world.gd
@@ -1,15 +1,31 @@
 extends Node2D
 
+# Main Variables
+
 var vx = 0
+var vy = 0
 
 
 func _process(delta: float):
+	# Identifying $Character as "c" for less typing
 	var c = $Character
+	
+	# Printing CatBunny's Current position for testing purposes (it's not meant to be visible in the game)
 	print("Character.x " + str(c.position.x))
+	print("Character.y " + str(c.position.y))
 	c.position.x += vx
+	c.position.y -= vy
+	
+	# Defending the borders so the CaBunny won't go off screen.
 	if c.position.x >= 315:
 		c.position.x -= 320
-	
+	if c.position.x <= -16:
+		c.position.x += 320
+	if c.position.y <= -22:
+		c.position.y += 240
+	if c.position.y >= 230:
+		c.position.y -= 240
+
 func _init():
 	pass
 
@@ -20,17 +36,36 @@ func _init():
 func _input(event: InputEvent):
 	var left = event.is_action_pressed("ui_left")
 	var right = event.is_action_pressed("ui_right")
+	var up = event.is_action_pressed("ui_up")
+	var down = event.is_action_pressed("ui_down")
 	var c = $Character
 	
+					# Walking Start
 	if left:
-		vx = -1
+		vx = -2
 		c.play()
 		
 	if right:
-		vx = 1
+		vx = 2
 		c.play()
+	
+	if up:
+		vy = 2
+		c.play()
+	
+	if down:
+		vy = -2
+		c.play()
+
+					# Walking stop
 	
 	if event.is_action_released("ui_left") \
 	   or event.is_action_released("ui_right"):
 		vx = 0
+		c.stop()
+
+
+	if event.is_action_released("ui_up") \
+	   or event.is_action_released("ui_down"):
+		vy = 0
 		c.stop()
